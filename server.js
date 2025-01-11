151eaea486708fb867280af6e81ecaa4e273cfb4
Kd526 <davisthoyah@gmail.com>
2025-01-11 18:22:42 +0300

    add style

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
-    res.send("listen to port")
+    res.render("homepage")
 })
 
 app.listen(3000)
