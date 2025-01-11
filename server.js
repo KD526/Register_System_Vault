0c08c9000aa1bae4216be32c37c85e37ff109134
Kd526 <davisthoyah@gmail.com>
2025-01-11 20:57:21 +0300

    config db

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express")
 const app = express()
 
 app.set("view engine", "ejs")
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static("public"))
+
+app.use(function (req, res, next) {
+    res.locals.errors = []
+    next()
+})
 
 app.get("/", (req, res) => {
     res.render("homepage")
@@ -24,11 +29,17 @@ app.post("/register", (req, res) => {
     if (req.body.username && req.body.username.length > 10) errors.push("Cannot be more than 10 characters!")
     if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Cn only have letters and numbers!")
 
+    if (!req.body.password) errors.push("Must provide password!")
+    if (req.body.password && req.body.password.length < 8) errors.push("password Must be atleast 8 characters!")
+    if (req.body.password && req.body.password.length > 20) errors.push("password Cannot be more than 20 characters!")
+
     if (errors.length) {
-        return res.render("homepage", {errors})
-    } else {
-        res.send("Thank you for registering!")
+        return res.render("homepage", { errors })
     }
+
+    //save user details into DB
+    
+
 
 })
 
